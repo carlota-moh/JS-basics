7731345d0db059a9c15d14ee0dba41cc78004b16
Carlota <carlota.monederoh@gmail.com>
2023-04-24 21:33:15 +0200

    Added exercises to practice.js

--- course/practice.js
+++ course/practice.js
@@ -18,7 +18,7 @@ function maxNumber (a, b) {
 // console.log(maxNumber(1, 1)) // return 1
 
 // isLandscape
-// return true if widht is greater than height and false otherwise
+// return true if width is greater than height and false otherwise
 
 function isLandscape(width, height){
     return (width > height);
@@ -76,3 +76,111 @@ function checkSpeed(speed, speedLimit=70, kmPerPoint=5) {
 // console.log(checkSpeed(80)); // Points: 2
 // console.log(checkSpeed(180)); // License suspended
 
+// showNumbers
+// // show whether a number is even or odd for all numbers below a limit
+
+function isEven(num) {
+    return (num % 2 == 0) ? 'EVEN': 'ODD';
+}
+
+function showNumbers(limit) {
+    for (let num = 0; num <= limit; num++) {
+        console.log(num, isEven(num))
+    };
+}
+
+// showNumbers(10);
+
+// countTruthy
+
+// take an array and count the number of truthy elements inside it
+// Falsy: undefined, null, '', false, 0, NaN
+
+function countTruthy(array) {
+    let count = 0;
+    for (element of array) {
+        if (element) count ++;
+    }
+    return count;
+}
+
+// console.log(countTruthy([0,2, 1,2,3]))
+
+// showProperties
+
+// Display all the properties within and object that
+// are of type string
+
+function showProperties(obj) {
+    for (let key in obj) {
+        if (typeof(obj[key]) === 'string') console.log(key, obj[key]);
+    }
+}
+
+// return the sum of all multiples of 3 and 5 up to a limit
+
+function sumUpToLimit(limit) {
+    let total = 0;
+    
+    for(let i = 0; i <= limit; i++) {
+        if (i % 5 == 0 || i % 3 == 0) total += i;
+    }
+
+    return total;
+}
+
+// calculate average grade from a list of marks
+// and format the output as follows:
+
+// 1-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+const marks = [80, 80, 50];
+
+function calculateGrade(marks) {
+    const averageGrade = marks.reduce((x, y) => x + y) / marks.length;
+    let gradeLetter = undefined;
+
+    if (averageGrade < 60) gradeLetter = "F";
+    else if (60 <= averageGrade < 70) gradeLetter = "D";
+    else if (70 <= averageGrade < 80) gradeLetter = "C";
+    else if (80 <= averageGrade < 90) gradeLetter = "B";
+    else gradeLetter = "A";
+
+    return {
+        grade: averageGrade, 
+        letter: gradeLetter
+    };
+}
+
+// show rows with stars. The number of stars should correspond
+// to the row number
+
+function showStars(rows) {
+    let stars = "";
+    for(let i = 1; i <= rows; i++) {
+        stars = stars.concat("*")
+        console.log(stars);
+    }
+}
+
+// create a function that shows all the prime
+// numbers up to a limit
+
+function checkPrimes(num) {
+    for (let factor = 2; factor < num; factor++)
+        if (num % factor === 0) return false;
+
+    return true
+
+}
+
+function showPrimes(limit) {
+    for (let num = 2; num <= limit; num++)
+        if(checkPrimes(num)) console.log(num);
+}
+
+showPrimes(7);
